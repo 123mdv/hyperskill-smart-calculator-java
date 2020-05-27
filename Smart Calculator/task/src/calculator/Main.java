ba1669cfbfff967e7e98e87fb39a186b861cbe2a
Maarten de Vries <123mdv@gmail.com>
2020-05-28 00:38:27 +0100

    Stage #3: Count them all

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
+        String input = "";
+
+        while (!"/exit".equals(input)) {
+            input = scanner.nextLine();
+            switch(input) {
+                case "/exit":
+                    System.out.println("Bye!");
+                    break;
+                case "":
+                    continue;
+                default:
+                    String[] inputArray = input.split(" ");
+                    if (inputArray.length == 1) {
+                        System.out.println(input);
+                        continue;
+                    }
+                    System.out.println(Integer.parseInt(inputArray[0]) +
+                            Integer.parseInt(inputArray[1]));
+            }
+        }
     }
 }
