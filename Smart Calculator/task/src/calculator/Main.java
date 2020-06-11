4fc751e1a867c61d468901c51723bdc85202378f
Maarten de Vries <123mdv@gmail.com>
2020-06-11 09:48:24 +0100

    Stage #5: Add errors

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -10,33 +10,47 @@ public class Main {
 
         while (!"/exit".equals(input)) {
             input = scanner.nextLine();
-            switch(input) {
-                case "/exit":
-                    System.out.println("Bye!");
-                    break;
-                case "/help":
-                    System.out.println("The program can calculate expressions with multiple additions and subtractions.\n" +
-                            "It supports unary and binary minus operators, as well as several operators following each other.");
-                    continue;
-                case "":
-                    continue;
-                default:
-                    System.out.println(multiSum(input));
+
+            if (input.startsWith("/")) {
+                switch (input) {
+                    case "/exit":
+                        System.out.println("Bye!");
+                        break;
+                    case "/help":
+                        System.out.println("The program can calculate expressions with multiple additions and subtractions.\n" +
+                                "It supports unary and binary minus operators, as well as several operators following each other.");
+                        break;
+                    default:
+                        System.out.println("Unknown command");
+                }
+            }
+            else if (!"".equals(input)){
+                System.out.println(multiSum(input));
             }
         }
     }
 
-    public static int multiSum(String input) {
+    public static String multiSum(String input) {
         // int answer = value of first item
         String[] inputArray = input.split("\\s+");
-        int answer = Integer.parseInt(inputArray[0]);
-        if (inputArray.length == 1) return answer;
+        int answer = 0;
 
-        for (int i = 1; i < inputArray.length; i += 2) {
-            answer = singleSum(answer, inputArray[i], Integer.parseInt(inputArray[i + 1]));
+        try {
+            answer = Integer.parseInt(inputArray[0]);
+
+            if (inputArray.length == 1) {
+                return (answer + "");
+            }
+
+            for (int i = 1; i < inputArray.length; i += 2) {
+                answer = singleSum(answer, inputArray[i], Integer.parseInt(inputArray[i + 1]));
+            }
+
+            return (answer + "");
         }
-
-        return answer;
+        catch (Exception e) {
+            return "Invalid expression";
+        }
     }
 
     public static int singleSum(int a, String operand, int b) {
